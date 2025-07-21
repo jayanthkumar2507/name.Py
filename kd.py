1bc0b8b14f6637536ed4ad8d5ae2e88ed6f0c9d4
jayanthkumar2507 <jayanthkumarmutha@gmail.com>
2025-07-21 22:17:09 +0530

    Add files via upload

---
+++ kd.py
@@ -0,0 +1,10 @@
+Name="Jayanth"
+Roll_No="96"
+subject="Python"
+clg="Sri Venkateshwara Engineering College"
+Department="C😗S😗D"
+print ("Name:",Name) 
+print ("Roll No:",Roll_No)
+print ("Subject:", subject) 
+print ( clg,(sep=",") ) 
+print ("Department:",Department) 
